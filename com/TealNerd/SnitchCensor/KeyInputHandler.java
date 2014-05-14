7d010c18cb915e60b0c10cdc2900039131886977
TealNerd <TealNerd@users.noreply.github.com>
2014-05-14 18:27:25 -0500

    Update KeyInputHandler.java

--- com/TealNerd/SnitchCensor/KeyInputHandler.java
+++ com/TealNerd/SnitchCensor/KeyInputHandler.java
@@ -9,10 +9,11 @@ public class KeyInputHandler {
 Minecraft mc = Minecraft.getMinecraft();
     @SubscribeEvent
     public void onKeyInput(InputEvent.KeyInputEvent event) {
-        if(SnitchCensor.toggle.isPressed() && SnitchCensor.isEnabled == false){
+        if(SnitchCensor.toggle.isPressed() && !SnitchCensor.isEnabled){
         	mc.thePlayer.addChatMessage(new ChatComponentText("SnitchCensor Enabled"));
         	SnitchCensor.isEnabled = true;
-        	}else if(SnitchCensor.toggle.isPressed() && SnitchCensor.isEnabled == true){
+        	}
+        if(SnitchCensor.toggle.isPressed() && SnitchCensor.isEnabled){
         	mc.thePlayer.addChatMessage(new ChatComponentText("SnitchCensor Disabled"));
         	SnitchCensor.isEnabled = false;
         	}
