cd8be63abdcdd1068af3f6bd436e6872772c5697
TealNerd <TealNerd@users.noreply.github.com>
2014-05-14 18:17:43 -0500

    Update SnitchCensor.java

--- com/TealNerd/SnitchCensor/SnitchCensor.java
+++ com/TealNerd/SnitchCensor/SnitchCensor.java
@@ -9,8 +9,14 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Properties;
 
+import org.lwjgl.input.Keyboard;
+
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler; // used in 1.6.2
 //import cpw.mods.fml.common.Mod.PreInit;    // used in 1.5.2
@@ -21,12 +27,21 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.InputEvent;
+import cpw.mods.fml.common.gameevent.InputEvent.KeyInputEvent;
 
-@Mod(modid="snitchcensor", name="Snitch Coordinate Censor", version="1.0.0")
+@Mod(modid="SnitchCensor", name="Snitch Coordinate Censor", version="1.1.0")
 
 public class SnitchCensor {
-
 	
+	Minecraft mc = Minecraft.getMinecraft();
+	static boolean isEnabled = false;
+	public static KeyBinding toggle;
+	
+	
+
+
     // The instance of your mod that Forge uses.
     @Instance(value = "SnitchCensor")
     public static SnitchCensor instance;
@@ -37,15 +52,21 @@ public class SnitchCensor {
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
     	SnitchCensor.instance = this;
-    	MinecraftForge.EVENT_BUS.register(new ChatParse());
+    	MinecraftForge.EVENT_BUS.register(new ChatFilter());
+    	FMLCommonHandler.instance().bus().register(new KeyInputHandler());
     }
+    
+    
     
     @EventHandler
     public void load(FMLInitializationEvent event) {
             proxy.registerListeners();
-            
-           
+            toggle = new KeyBinding("Toggle Censor", Keyboard.KEY_T, "Snitch Censor");
+            ClientRegistry.registerKeyBinding(toggle);
     }
+    
+    
+    
     
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
