82ff151fffa406d8d4bb4ee134d589f12a157428
TealNerd <TealNerd@users.noreply.github.com>
2014-05-14 18:17:00 -0500

    Create KeyInputHandler.java

---
+++ com/TealNerd/SnitchCensor/KeyInputHandler.java
@@ -0,0 +1,21 @@
+package com.TealNerd.SnitchCensor;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.InputEvent;
+
+public class KeyInputHandler {
+Minecraft mc = Minecraft.getMinecraft();
+    @SubscribeEvent
+    public void onKeyInput(InputEvent.KeyInputEvent event) {
+        if(SnitchCensor.toggle.isPressed() && SnitchCensor.isEnabled){
+        	mc.thePlayer.addChatMessage(new ChatComponentText("SnitchCensor Enabled"));
+        	SnitchCensor.isEnabled = true;
+        	}else if(SnitchCensor.toggle.isPressed() && SnitchCensor.isEnabled == true){
+        	mc.thePlayer.addChatMessage(new ChatComponentText("SnitchCensor Disabled"));
+        	SnitchCensor.isEnabled = false;
+        	}
+    }
+
+}
